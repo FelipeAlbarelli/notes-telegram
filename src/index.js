d0cc76aeca16e35106e8b288eb2c57957c35b31e
Felipe Albarelli <felipesalbarelli@gmail.com>
2020-04-11 18:28:08 -0300

    aded help command

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-const express = require('express')
+// const express = require('express')
 require('./db/mongoose') //connect to db
-const app = express();
-const port  = process.env.PORT || 3000;
-const userRouter = require('./routers/users')
+// const app = express();
+// const port  = process.env.PORT || 3000;
+// const userRouter = require('./routers/users')
 const botgram = require('botgram')
 const User = require('./models/users')
 const Notes = require('./models/notes')
@@ -12,8 +12,49 @@ const bot    = botgram(botKey)
 
 bot.context({
     cacheArray : [],
-    confirmed : false
+    confirmed : false,
+    looksForHelp : false
 });
+
+// middleware for all commands, check if 'looksForHelp', if true, then shows info about command instead
+bot.command( /\w*/ , (msg , reply , next) => {
+    reply.keyboard(false)
+
+    if (msg.context.looksForHelp){
+        switch (msg.text){
+            case ('/add') :
+                reply.text('/add #tag# texto' 
+                + '\n' 
+                + 'Adiciona uma anotação com uma tag. A tag deve estar entre dois "#" e é opcional. Notas com tags podem ser mais facilmente acessadas pelo comando /list.')
+                break
+            case ('/list') :
+                reply.text('/list #tag#' 
+                + '\n' 
+                + 'Lista todas as anotações com a tag. Se não for fornecida uma tag, todas as anotações são listadas.'
+                + '\n'
+                + 'O índice i dado a cada anotação pode ser usado para remove-las usando o comando "/remove i".')
+                break
+            case '/remove' :
+                reply.text('/remove i'
+                + '\n'
+                + 'Remove a anotação de índice "i" da última listagem fornecida pelo comando "/list".')
+                break 
+            case '/removeAll' :
+                reply.text('/removeAll #tag#'
+                + '\n'
+                + 'Remove todas as anotações com a tag fornecida. Caso não seja fornecida uma tag, TODAS as notas são apagadas.'
+                + '\n'
+                + 'Em qualquer um dos casos, é feito um pedido de confirmação.')
+                break
+            default:
+                reply.text('commando desconhecido')
+                break
+        }
+        msg.context.looksForHelp = false;
+    } else {
+        next()
+    }
+})
 
 bot.command('start' , async (msg , reply , next) => {
     const {username , id : telegramId , name} = msg.from;
@@ -35,7 +76,6 @@ bot.command('start' , async (msg , reply , next) => {
 })
 
 bot.text( (msg , reply , next) =>{
-    console.log(msg.context.cacheArray)
 })
 
 bot.command('add' , async (msg , reply , next) => {
@@ -81,15 +121,25 @@ bot.command('remove' , async (msg , reply , next) => {
     }
 })
 
-bot.command('test' , (msg , reply , next) => {
+bot.command('help' , (msg , reply , next) => {
+
+    msg.context.looksForHelp = true;
+
+    const keys =  [
+        [ '/add' , '/list'],
+        ['/remove' , '/removeAll']
+    ]
+    reply.keyboard(keys).text("Clique em um comando para saber mais sobre este.")
 
 })
 
-app.use(express.json())
-app.use(userRouter)
 
-app.listen(port , () => {
-    console.log('-----------------------------------')
-    console.log('server is up on port' , port)
-})
+// app.use(express.json())
+// app.use(userRouter)
 
+// app.listen(port , () => {
+//     console.log('-----------------------------------')
+//     console.log('server is up on port' , port)
+// })
+
+console.log('-'.repeat(20))
